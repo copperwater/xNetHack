0fe43993e6aa4bd766ea9c5858510eeebe4aec7d
copperwater <aosdict@gmail.com>
2017-08-17 21:25:03 -0400

    Fix missing comma

--- src/polyself.c
+++ src/polyself.c
@@ -1612,7 +1612,7 @@ int part;
     /* claw attacks are overloaded in mons[]; most humanoids with
        such attacks should still reference hands rather than claws */
     static const char not_claws[] = {
-        S_HUMAN,     S_MUMMY,   S_ZOMBIE, S_ANGEL, S_NYMPH
+        S_HUMAN,     S_MUMMY,   S_ZOMBIE, S_ANGEL, S_NYMPH,
         S_QUANTMECH, S_VAMPIRE, S_ORC,    S_GIANT, /* quest nemeses */
         '\0' /* string terminator; assert( S_xxx != 0 ); */
     };
