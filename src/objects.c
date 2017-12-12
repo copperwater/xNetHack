54116332015da9bed8af6d70ee81384f26104823
copperwater <aosdict@gmail.com>
2017-12-12 15:15:27 -0500

    Touchstones are no longer magical
    Based on real-life touchstones being merely dark rocks that a skilled
    user can use to determine the makeup of the things it's being rubbed on.

--- src/objects.c
+++ src/objects.c
@@ -1098,7 +1098,7 @@ GEM("worthless piece of violet glass", "violet",
            0, GEM_CLASS, prob, 0, wt, gval, sdam, ldam, 0, 0, nutr, color)
 ROCK("luckstone", "gray",  0,  10,  10, 60, 3, 3, 1, 10, 7, 1, 0, MINERAL, CLR_GRAY),
 ROCK("loadstone", "gray",  0,  10, 500,  1, 3, 3, 1, 10, 6, 1, 0, MINERAL, CLR_GRAY),
-ROCK("touchstone", "gray", 0,   8,  10, 45, 3, 3, 1, 10, 6, 1, 0, MINERAL, CLR_GRAY),
+ROCK("touchstone", "gray", 0,   8,  10, 45, 3, 3, 0, 10, 6, 1, 0, MINERAL, CLR_GRAY),
 ROCK("thiefstone", "gray", 0,  10,  10, 60, 3, 3, 1, 10, 6, 0, 1, MINERAL, CLR_GRAY),
 ROCK("flint", "gray",      0,  10,   4,  1, 6, 6, 0, 10, 7, 1, 0, MINERAL, CLR_GRAY),
 ROCK("rock", None,         1, 100,  10,  0, 3, 3, 0, 10, 7, 1, 0, MINERAL, CLR_GRAY),
