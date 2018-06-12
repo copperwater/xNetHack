6b0efca286ac8174799c63962f4a2febff42769a
copperwater <aosdict@gmail.com>
2018-06-12 11:33:36 -0400

    Also print a message when the hero level teleports

--- src/teleport.c
+++ src/teleport.c
@@ -758,8 +758,8 @@ level_tele()
             killer.format = NO_KILLER_PREFIX;
             Strcpy(killer.name, "went to heaven prematurely");
         } else if (newlev == -9) {
-            You_feel("deliriously happy. ");
-            pline("(In fact, you're on Cloud 9!) ");
+            You_feel("deliriously happy.");
+            pline("(In fact, you're on Cloud 9!)");
             display_nhwindow(WIN_MESSAGE, FALSE);
         } else
             You("are now high above the clouds...");
@@ -833,6 +833,7 @@ level_tele()
        call it something, we can't defer until the end of the turn */
     if (u.utotype && !context.mon_moving)
         deferred_goto();
+    You("materialize on a different level!");
 }
 
 void
