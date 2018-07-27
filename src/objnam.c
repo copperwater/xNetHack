a8f86ca924353803376f7198046fd717290fe7d6
copperwater <aosdict@gmail.com>
2018-07-27 15:22:45 -0400

    Fix: wishing for the Platinum Yendorian Express Card didn't work
    Due to the "platinum" being parsed as a material.

--- src/objnam.c
+++ src/objnam.c
@@ -3061,7 +3061,8 @@ struct obj *no_wish;
         } else {
             /* check for materials */
             if (!strncmpi(bp, "silver dragon", l = 13)
-                || !strcmp(bp, "gold")) {
+                || !strcmp(bp, "gold")
+                || !strncmpi(bp, "platinum yendorian express card", l = 31)) {
                 /* hack so that silver dragon scales/mail doesn't get
                  * interpreted as silver, or a wish for just "gold" doesn't get
                  * interpreted as gold */
