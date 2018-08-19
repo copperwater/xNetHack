b746c2391303d4e3351b93064021ffd6ce0012b7
copperwater <aosdict@gmail.com>
2018-08-18 23:25:14 -0400

    Livelog when the player kills their own pet
    This is normally a fairly rare event because of its harsh penalty and
    lack of any real benefit, so it seems like it wouldn't be spammy to
    livelog it. I may be wrong on this, but there's only one way to find
    out.
    
    If you have livelog configured and wish to livelog this target, make
    sure your LIVELOGFLAGS in the sysconf are not set to the default 0xFF,
    because the flag for this livelog is 0x0100.

--- src/mon.c
+++ src/mon.c
@@ -2555,6 +2555,10 @@ cleanup:
             You_hear("the rumble of distant thunder...");
         else
             You_hear("the studio audience applaud!");
+        if (!unique_corpstat(mdat) && has_mname(mtmp)) {
+            livelog_printf(LL_KILLEDPET, "murdered %s, %s faithful %s",
+                           mon_nam(mtmp), uhis(), mdat->mname);
+        }
     } else if (mtmp->mpeaceful)
         adjalign(-5);
 
