f601f232f0c9519907daac53f5b4a4e5de4a3b9a
copperwater <aosdict@gmail.com>
2019-09-20 00:25:41 -0400

    Glass body armor can shatter if hit with a heavy flying object
    Subject to the usual stuff; shatterproof armor won't break, etc. The
    criterion for "heavy" is the same as for objects dropping on your helm:
    more than 400 weight. In practice this should only be boulders.
    
    It does work with rolling boulder traps, since those count as flying
    objects.

--- src/mthrowu.c
+++ src/mthrowu.c
@@ -110,6 +110,9 @@ const char *name; /* if null, then format `*objp' */
                 }
                 exercise(A_CON, FALSE);
             }
+            else if (obj && obj->owt >= 400 && is_brittle(uarm)) {
+                break_glass_obj(uarm);
+            }
             if (is_acid)
                 pline("It burns!");
             losehp(dam, knm, kprefix); /* acid damage */
