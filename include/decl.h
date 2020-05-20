55badbce8c3e8c53afff3b0a9fe4755896fdf01f
k21971 <keith.simpson1971@gmail.com>
2020-05-20 20:24:28 +0000

    Remove multiple definition of 'whereis_real_path'.
    Caught during compile using gcc 10.

--- include/decl.h
+++ include/decl.h
@@ -181,10 +181,6 @@ struct kinfo {
     char name[BUFSZ]; /* actual killer name */
 };
 
-#ifdef WHEREIS_FILE
-    char whereis_real_path[255];
-#endif
-
 E const schar xdir[], ydir[], zdir[];
 
 struct multishot {
