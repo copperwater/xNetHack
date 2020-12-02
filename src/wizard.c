copperwater/xNetHack

commit 18a5bacbf73166182ed2b4990144c40bcbfaf52d

Adjust the formula for the number of summoned nasties
Another Fourk change. This formula change also introduces a related
Fourk feature, which is that summon nasties' monster count should scale
based off the caster's level, not the player's. (Why should Rodney and
liches be kinder to a low-level pacifist player?)

The goal of the formula change is to make the summon nasties range a bit
less swingy, where level 30 could produce as many as 10 or as few as 1
monster.

The new formula makes the maximum number of possible monsters scale up
more slowly with respect to level, but increases the minimum to 3
instead of 1. (Fourk sets the minimum at 4 which I felt was a bit too
high.) A level 30 Wizard will generate about the same amount of nasties
as he did previously; mid-level casters will generate slightly more.

src/wizard.c
@@ -585,12 +585,12 @@ struct monst *summoner;
         s_cls = summoner ? summoner->data->mlet : 0;
         difcap = summoner ? summoner->data->difficulty : 0; /* spellcasters */
         castalign = summoner ? sgn(summoner->data->maligntyp) : 0;
-        tmp = (u.ulevel > 3) ? u.ulevel / 3 : 1;
+        tmp = 2 + rnd((summoner->m_lev / 5) + 1);
         /* if we don't have a casting monster, nasties appear around hero,
            otherwise they'll appear around spot summoner thinks she's at */
         bypos.x = u.ux;
         bypos.y = u.uy;
-        for (i = rnd(tmp); i > 0 && count < MAXNASTIES; --i) {
+        for (i = tmp; i > 0 && count < MAXNASTIES; --i) {
             /* Of the 44 nasties[], 10 are lawful, 14 are chaotic,
              * and 20 are neutral.  [These numbers are up date for
              * 3.7.0; the ones in the next paragraph are not....]

commit be94471e813c4b5e282938edae7b23973bcaf5a6

Fix: segfault when Wizard of Yendor harassment summoned nasties
Introduced in 18a5bac when I changed the formula here to use the
summoner's level instead of the player's, and evidently forgot that
nasty() can be called with no summoner during harassment.

Now, if the summoner is absent, nasty() will use a monster level of 30,
equal to the Wizard's starting level. I went with a constant value here
because it's simpler than duplicating a code check for what the last
incarnation of the Wizard had as a m_lev, and this makes the real Wizard
nominally more dangerous than his harassment summoning.

src/wizard.c
@@ -585,7 +585,7 @@ struct monst *summoner;
         s_cls = summoner ? summoner->data->mlet : 0;
         difcap = summoner ? summoner->data->difficulty : 0; /* spellcasters */
         castalign = summoner ? sgn(summoner->data->maligntyp) : 0;
-        tmp = 2 + rnd((summoner->m_lev / 5) + 1);
+        tmp = 2 + rnd(((summoner ? summoner->m_lev : 30) / 5) + 1);
         /* if we don't have a casting monster, nasties appear around hero,
            otherwise they'll appear around spot summoner thinks she's at */
         bypos.x = u.ux;