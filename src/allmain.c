c0bf74b23638f6b9cf4b8b2cf13032539144f752
copperwater <aosdict@gmail.com>
2021-06-01 13:43:35 -0400

    Fix: serverseed varied between first and subsequent game sessions
    Due to there being a large random number added in newgame(), which
    wasn't saved at all, when the game was reloaded it would then use
    just sysopt.serverseed without this number. This affects not just
    readable Hawaiian shirts but also things like which trap is on a door,
    so I needed to fix it in some way given Junethack is running.
    
    Temporary fix is to remove the large random number so that serverseed is
    just in effect all the time. A proper fix would require a savebreak.

--- src/allmain.c
+++ src/allmain.c
@@ -676,7 +676,7 @@ newgame(void)
     g.context.tribute.tributesz = sizeof(struct tribute_info);
 
     /* Extra entropy added to sysopt.serverseed */
-    sysopt.serverseed += rn2(8000000);
+    /* sysopt.serverseed += rn2(8000000); */
 
     for (i = LOW_PM; i < NUMMONS; i++)
         g.mvitals[i].mvflags = mons[i].geno & G_NOCORPSE;
