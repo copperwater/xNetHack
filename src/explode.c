ee4a8e7b78da4b53c623d4a0c93461b2237a50a7
Michael Meyer <me@entrez.cc>
2021-10-18 16:41:24 -0400

    Fix: thrown oil/exploding door death reason
    Being killed by an explosion from a thrown potion of oil or a
    FIRE_BLAST door trap would give something like "caught herself in her
    own fireball" as the cause of death.  Revise those messages so they
    don't blame the poor dead hero for something she didn't do.

--- src/explode.c
+++ src/explode.c
@@ -31,7 +31,8 @@ explode(
     int x, int y, /* explosion's location; adjacent spots are also affected */
     int type,     /* same as in zap.c; -(wand typ) for some WAND_CLASS */
     int dam,      /* damage amount */
-    char olet,    /* object class or BURNING_OIL or MON_EXPLODE */
+    char olet,    /* object class or BURNING_OIL or MON_EXPLODE or
+                     TRAPPED_DOOR */
     int expltype) /* explosion type: controls color of explosion glyphs */
 {
     int i, j, k, damu = dam;
@@ -567,11 +568,16 @@ explode(
                 else if (str != g.killer.name && str != hallu_buf)
                     Strcpy(g.killer.name, str);
                 g.killer.format = KILLED_BY_AN;
+            } else if (olet == TRAPPED_DOOR || olet == BURNING_OIL) {
+                g.killer.format = KILLED_BY_AN;
+                Snprintf(g.killer.name, sizeof g.killer.name,
+                         "exploding %s",
+                         olet == BURNING_OIL ? "fire bomb" : "door");
             } else if (type >= 0 && olet != SCROLL_CLASS) {
                 g.killer.format = NO_KILLER_PREFIX;
-                    Snprintf(g.killer.name, sizeof g.killer.name,
-                             "caught %sself in %s own %s", uhim(),
-                        uhis(), str);
+                Snprintf(g.killer.name, sizeof g.killer.name,
+                         "caught %sself in %s own %s", uhim(),
+                         uhis(), str);
             } else {
                 g.killer.format = (!strcmpi(str, "tower of flame")
                                     || !strcmpi(str, "fireball"))
