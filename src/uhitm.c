f722837f6d9b4dbb470efe22cd8b77afeb87b7c2
copperwater <aosdict@gmail.com>
2022-03-08 17:28:50 -0500

    Fix: double livelog of breaking weaponless conduct
    Vanilla appears to have implemented the livelog for this differently
    from how the livelog patch did it, leading to a double message. There
    may be other ones like this lurking around, but I didn't see any other
    livelogs that were given their own functions like this one, so I don't
    know of any.

--- src/uhitm.c
+++ src/uhitm.c
@@ -638,9 +638,6 @@ known_hitum(
             if (mon->wormno && *mhit)
                 cutworm(mon, g.bhitpos.x, g.bhitpos.y, slice_or_chop);
         }
-        if (u.uconduct.weaphit && !oldweaphit)
-            livelog_printf(LL_CONDUCT,
-                           "hit with a wielded weapon for the first time");
     }
     return malive;
 }
