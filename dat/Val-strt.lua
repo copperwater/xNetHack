7cdf22f62f230d130186153c6762957ab5ce8a5a
copperwater <aosdict@gmail.com>
2022-03-18 07:44:11 -0400

    Implement redesigned Valkyrie home level
    This is not a particularly radical departure from what it was. Still a
    shrine in a field of ice, same warriors, same lava vents. That said,
    differences:
    - The building is more interesting than just a big rectangle. Its
      external doors are also iron so the marauding giants don't bash them
      down right away.
    - The hero proceeds across the map from left to right as is standard in
      my redesigned quest levels.
    - Different treasure; now a guaranteed spellbook of cone of cold, a good
      small shield and good spear (potential replacements for starting
      gear), as well as a few extra random weapons and armor. But no random
      chest contents.
    - There are frost giants as well; Ragnarok involves them as well as fire
      giants.
    - I also added fire vortices. There isn't any particular reason that
      fire ants should be the only Muspell grunt troops.

--- dat/Val-strt.lua
+++ dat/Val-strt.lua
@@ -10,92 +10,129 @@
 --	and receive your quest assignment.
 --
 
-des.level_flags("mazelevel", "noteleport", "hardfloor", "icedpools", "outdoors")
+des.level_flags("mazelevel", "noteleport", "hardfloor", "icedpools", "outdoors", "noflipx")
 des.level_init({ style = "solidfill", fg = "I" })
 
-local pools = selection.new()
--- random locations
-for i = 1,13 do
-   pools:set();
-end
--- some bigger ones
-pools = pools | selection.grow(selection.set(selection.new()), "west")
-pools = pools | selection.grow(selection.set(selection.new()), "north")
-pools = pools | selection.grow(selection.set(selection.new()), "random")
-
--- Lava pools surrounded by water
-des.terrain(pools:clone():grow("all"), "P")
-des.terrain(pools, "L")
-
 des.map([[
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxxx..xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx...xxxxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxx..xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx..{..xxxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxx..xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.....xxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxx..xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.xxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxx..xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.xxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxx..xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx..xxxxxxxxxxxxxxxxxxx
-xxxxxxxx.....xxxxxxxxxxxxx|----------------|xxxxxxxxxxx.xxxxxxxxxxxxxxxxxxxx
-xxxxxxx..xxx...xxxxxxxxxxx|................|xxxxxxxxxx..xxxxxxxxxxxxxxxxxxxx
-xxxxxx..xxxxxx......xxxxx.|................|.xxxxxxxxx.xxxxxxxxxxxxxxxxxxxxx
-xxxxx..xxxxxxxxxxxx.......+................+...xxxxxxx.xxxxxxxxxxxxxxxxxxxxx
-xxxx..xxxxxxxxx.....xxxxx.|................|.x...xxxxx.xxxxxxxxxxxxxxxxxxxxx
-xxx..xxxxxxxxx..xxxxxxxxxx|................|xxxx.......xxxxxxxxxxxxxxxxxxxxx
-xxxx..xxxxxxx..xxxxxxxxxxx|----------------|xxxxxxxxxx...xxxxxxxxxxxxxxxxxxx
-xxxxxx..xxxx..xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx...xxxxxxxxxxxxxxxxx
-xxxxxxx......xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx...xxxxxxxxxxxxxxx
-xxxxxxxxx...xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx...x......xxxxxx
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.........xxxxx
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.......xxxxxx
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIII...IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII...IIIIIIIII
+IIIIIIIIIIIIIII.....IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII.......IIIIIIII
+IIIIIIIIIIIIIII....IIIIIIIIIIIIIIIIIII-----IIIIIIIIIIIIII.....II...IIIIIIIII
+IIIIIIIIIIIIIII.IIIIIIIIIIIIII--------|...|IIIIIIIIII.....IIIIIII.IIIIIIIIII
+IIIIIIIIIIIIIII.IIIIIIIIIIIIII|.|.|.|.|...|IIIIIIIIII.IIIIIIIIIII.IIIIIIIIII
+IIIIIIIIIIIIIII..IIIIIIIIIIIII|.......|...|IIIIIIIIII.IIIIIIIIIII..IIIIIIIII
+IIIIIIIIIIIIIIII.IIIIIIIIIIII-----+-----+----IIIIIIII.IIIIIIIIIIII.IIIIIIIII
+IIIIIIIIIIIIIIII..IIIIIIIII.--..............--IIIIIII..IIIIIIIIIII.IIIIIIIII
+IIIIIIIIIIIIIIIII.IIIIIIII..|................|IIIIIIII.IIIIIIIIIII..IIIIIIII
+IIIIIIIIIIIIIIIII.IIIII.....+................|IIIIIIII.IIIIIIIIIIII.IIIIIIII
+IIIIIIIIIIIIIIIII.III...II..|................|IIIII...........IIIII.IIIIIIII
+IIIIIIIIIIIIIIIII.....IIIII.--..............--......IIIIIIIII.......IIIIIIII
+IIIIIIIIIIIIIII...IIIIIIIIIII--------.-----+-..IIIIIIIIIIIIIIIIIIII...IIIIII
+IIIIIIIIIIIII...IIIIIIIIIIIIIIIIIII--.--II....IIIIIIIIIIIIIIIIIIIIIII...IIII
+IIIIIIIIIII...IIIIIIIIIIIIIIIIIIII--...--IIIIIIIIIIIIIIIIIIIIIIIIIIIIII.....
+I.......I...IIIIIIIIIIIIIIIIIIIIII|..T..|IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+..........IIIIIIIIIIIIIIIIIIIIIIII--...--IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+I........IIIIIIIIIIIIIIIIIIIIIIIIII-----IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
 ]]);
 -- Dungeon Description
-des.region(selection.area(00,00,75,19), "lit")
-des.region(selection.area(27,08,42,12), "lit")
+local everything = selection.area(00,00,75,19)
+local alltheice = everything:filter_mapchar('I')
+local inside = selection.floodfill(34,10) + selection.floodfill(34,06) + selection.floodfill(40,06)
+local outside = (everything:filter_mapchar('.') + alltheice) - inside
+local init_monster_area = outside & selection.area(17,00,75,19):grow()
+des.region(everything, "lit")
+-- Lava vents surrounded by water
+local pools = selection.new()
+local function growths()
+   -- Compute the number of times a random lava vent should grow in size.
+   if percent(40) then
+      return 1 + growths()
+   end
+   return 0
+end
+for i = 1,13 do
+   local x,y = alltheice:rndcoord()
+   local newpool = selection.new():set(x, y)
+   -- maybe turn into a bigger one
+   for j=1,growths() do
+      newpool = newpool:grow('random')
+   end
+   pools = pools | newpool
+end
+-- Only let them affect ice.
+local water = pools:clone():grow("all")
+des.terrain(water & alltheice, "P")
+des.terrain(pools & alltheice, "L")
+-- And now there's less ice.
+alltheice = alltheice - water
+init_monster_area = init_monster_area - water
+
 -- Portal arrival point
-des.levregion({ region = {66,17,66,17}, type="branch" })
+des.levregion({ region = {02,17,02,17}, type="branch" })
 -- Stairs
-des.stair("down", 18,01)
-des.feature("fountain", 53,02)
+des.stair("down", 75, 15)
+-- One fountain
+if percent(50) then
+   des.feature("fountain", 65,02)
+else
+   des.feature("fountain", 17,02)
+end
 -- Doors
-des.door("locked",26,10)
-des.door("locked",43,10)
+des.door({ state="locked", coord={28,10}, iron=1 })
+des.door("closed",34,07)
+des.door("locked",40,07)
+des.door({ state="locked", coord={43,13}, iron=1 })
 -- Norn
-des.monster({ id = "Norn", coord = {35, 10}, inventory = function()
+des.monster({ id = "Norn", coord = {42, 10}, inventory = function()
    des.object({ id = "banded mail", spe = 5 });
    des.object({ id = "long sword", spe = 4 });
 end })
 -- The treasure of the Norn
-des.object("chest", 36, 10)
--- valkyrie guards for the audience chamber
-des.monster("warrior", 27, 08)
-des.monster("warrior", 27, 09)
-des.monster("warrior", 27, 11)
-des.monster("warrior", 27, 12)
-des.monster("warrior", 42, 08)
-des.monster("warrior", 42, 09)
-des.monster("warrior", 42, 11)
-des.monster("warrior", 42, 12)
+des.object({ id="chest", x=43, y=08, contents = function()
+   des.object({ class='+', id='cone of cold' })
+end})
+des.object({ id="spear", coord={39,04}, spe=d(3) })
+des.object(")", 39, 05)
+des.object(")", 39, 06)
+des.object({ id="small shield", coord={41,04}, spe=3 })
+des.object("[", 41, 05)
+des.object("[", 41, 06)
+-- Valkyrie guards posted outside
+des.monster({ id="warrior", coord={27,09}, waiting=1 })
+des.monster({ id="warrior", coord={27,11}, waiting=1 })
+-- Valkyrie guards inside the hall
+des.monster("warrior", 34, 09)
+des.monster("warrior", 34, 11)
+des.monster("warrior", 40, 09)
+des.monster("warrior", 40, 11)
+des.monster({ id="warrior", coord={44,09}, waiting=1 })
+des.monster({ id="warrior", coord={44,11}, waiting=1 })
+-- Guards off shift in the bunkroom
+des.monster({ id="warrior", coord={33,05}, asleep=1 })
+des.monster({ id="warrior", coord={37,05}, asleep=1 })
+-- 
 -- Non diggable walls
-des.non_diggable(selection.area(26,07,43,13))
+des.non_diggable(selection.area(26,03,45,18))
 -- Random traps
-des.trap("fire")
-des.trap("fire")
-des.trap("fire")
-des.trap("fire")
-des.trap("fire")
-des.trap("fire")
+for i=1,8 do
+   des.trap({ type="fire", coord={alltheice:rndcoord(1)} })
+end
+-- Scatter a few boulders too (extra ammo for giants!)
+for i=1,d(3)+2 do
+   des.object({ id="boulder", coord={alltheice:rndcoord(1)} })
+end
 -- Monsters on siege duty.
-des.monster("fire ant", 04, 12)
-des.monster("fire ant", 08, 08)
-des.monster("fire ant", 14, 04)
-des.monster("fire ant", 17, 11)
-des.monster("fire ant", 24, 10)
-des.monster("fire ant", 45, 10)
-des.monster("fire ant", 54, 02)
-des.monster("fire ant", 55, 07)
-des.monster("fire ant", 58, 14)
-des.monster("fire ant", 63, 17)
-des.monster({ id = "fire giant", x=18, y=01, peaceful = 0 })
-des.monster({ id = "fire giant", x=10, y=16, peaceful = 0 })
-
+for i=1,d(2) do
+   des.monster({ id='fire ant', coord={init_monster_area:rndcoord(1)} })
+end
+for i=1,d(2)-1 do
+   des.monster({ id='fire vortex', coord={init_monster_area:rndcoord(1)} })
+end
+for i=1,d(3) do
+   des.monster({ id='fire giant', coord={init_monster_area:rndcoord(1)} })
+end
+-- Ragnarok doesn't only feature fire giants.
+for i=1,d(3) do
+   des.monster({ id='frost giant', coord={init_monster_area:rndcoord(1)} })
+end
