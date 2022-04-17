0abd7c2b6637a71fafbcafd64a2405db71e92337
copperwater <aosdict@gmail.com>
2022-04-17 19:09:14 -0400

    Redesign Valkyrie quest upper filler level
    Not an enormous change, but the lava vents persist, I threw in an
    unfrozen lake for a change of scenery, and it's no longer a mines-style
    filler. Plus the obligatory stairs at either end.
    
    Also experimenting with having no "loot" lying around on the level but
    instead carried by the filler monsters. So if you run around the giants
    and don't fight them, no items for you. (This also makes more sense than
    having random things lying around in the frozen wasteland.)

--- dat/Val-fila.lua
+++ dat/Val-fila.lua
@@ -3,38 +3,97 @@
 --	Copyright (c) 1991-2 by M. Stephenson
 -- NetHack may be freely redistributed.  See license for details.
 --
-des.level_init({ style = "solidfill", fg = "I" });
+des.level_flags("mazelevel", "outdoors", "icedpools", "noflipx")
 
-des.level_flags("mazelevel", "icedpools", "noflip")
+-- having a map of solid ice rather than level_init is a hack to set the
+-- relative coordinate origin to something sane rather than off the map at the
+-- in-game 0,0
+des.map([[
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+]])
 
-des.level_init({ style="mines", fg=".", bg="I", smoothed=true, joined=true, lit=1, walled=false })
+local everything = selection.area(00,00,75,19)
+des.region(everything, "lit")
+des.replace_terrain({ selection=everything:percentage(20), fromterrain='I', toterrain='.' })
 
---
-des.stair("up")
-des.stair("down")
---
-des.object()
-des.object()
-des.object()
-des.object()
-des.object()
-des.object()
-des.object()
-des.object()
-des.object()
---
+-- Stairs on opposite ends
+des.stair({ coord={selection.area(00,00,00,19):rndcoord()}, dir="up" })
+des.stair({ coord={selection.area(75,00,75,19):rndcoord()}, dir="down" })
+
+-- More random lava vents like in home level
+local middleground = selection.area(08,00,68,19)
+local pools = selection.new()
+for i = 1,15 do
+   local x,y = middleground:rndcoord()
+   local newpool = selection.new():set(x, y)
+   -- maybe turn into a bigger one
+   for j = 1, d(8)-5 do
+      newpool = newpool:grow('random')
+   end
+   pools = pools | newpool
+end
+local water = pools:clone():grow("all")
+des.replace_terrain({ selection=water, fromterrain='I', toterrain='P' })
+des.terrain(pools, "L")
+
+-- Unfrozen lake
+lakex = 20 + nh.rn2(25)
+lakey = 19
+xrad = 6
+yrad = 2
+lake = selection.ellipse(lakex, lakey, xrad, yrad, 1)
+lake = lake + selection.ellipse(lakex, lakey, xrad + 1, yrad + 1, 1):percentage(70)
+lake = lake + selection.ellipse(lakex, lakey, xrad + 2, yrad + 2, 1):percentage(40)
+des.terrain(lake, '}')
+-- remove random separated patches of water
+des.terrain(lake - selection.floodfill(lakex, lakey), 'I')
+
+-- A few random boulders (giants will probably pick some up)
+for i = 1,5+d(3) do
+   des.object('boulder')
+end
+
+-- Monsters
 des.monster("fire ant")
-des.monster("fire ant")
-des.monster("fire ant")
-des.monster("fire ant")
-des.monster("fire ant")
-des.monster("a")
-des.monster({ id = "fire giant", peaceful = 0 })
---
-des.trap()
-des.trap()
-des.trap()
-des.trap()
-des.trap()
-des.trap()
-des.trap()
+for i = 1,2+d(3) do
+   -- No objects scattered on the floor here, instead they are in the giants'
+   -- inventories.
+   template = { peaceful = 0, inventory = function()
+      for j = 1,d(2) do
+         des.object()
+      end
+   end }
+   local v = math.random(0,99)
+   if v < 45 then
+      template['id'] = 'fire giant'
+   elseif v < 90 then
+      template['id'] = 'frost giant'
+   else
+      template['class'] = 'H'
+   end
+   des.monster(template)
+end
+
+-- Random traps
+for i=1,6+d(3) do
+   des.trap({ spider_on_web = false })
+end
