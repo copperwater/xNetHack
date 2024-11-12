724f8f865c3b6ad6a8cfade32ae1ee2708f90c5b
nhmall <nhmall@nethack.org>
2024-11-12 18:51:04 -0500

    more memory freeing before exit on Windows

--- sys/windows/consoletty.c
+++ sys/windows/consoletty.c
@@ -1131,6 +1131,8 @@ consoletty_exit(void)
     /* go back to using the safe routines */
     safe_routines();
     free_custom_colors();
+    free((genericptr_t) console.front_buffer);
+    free((genericptr_t) console.back_buffer);
     free((genericptr_t) console.localestr);
     free((genericptr_t) console.orig_localestr);
 }
