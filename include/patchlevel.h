21d35e768c2bbc9126c931331b30088f4a4d7192
nhmall <nhmall@nethack.org>
2024-12-19 17:54:55 -0500

    bump EDITLEVEL for previous two commits DEC19-2024

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -17,7 +17,7 @@
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
  */
-#define EDITLEVEL 113
+#define EDITLEVEL 114
 
 /*
  * Development status possibilities.
