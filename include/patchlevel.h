bda3437eab1585bfeba5425c11f8e7d80c4a190e
nhmall <nhmall@nethack.org>
2025-02-02 09:00:21 -0500

    bump EDITLEVEL

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -17,7 +17,7 @@
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
  */
-#define EDITLEVEL 115
+#define EDITLEVEL 116
 
 /*
  * Development status possibilities.
