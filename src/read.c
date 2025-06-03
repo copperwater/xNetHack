copperwater/xNetHack

commit 9cd266ae5fb312fbfca827df32e299881c9498b8

Make the blessed scroll of gold detection also detect gems

src/read.c
@@ -2136,8 +2136,16 @@ seffect_gold_detection(struct obj **sobjp)
     struct obj *sobj = *sobjp;
     boolean scursed = sobj->cursed;
     boolean confused = (Confusion != 0);
+    boolean failure;
 
-    if ((confused || scursed) ? trap_detect(sobj) : gold_detect(sobj, FALSE))
+    if (confused || scursed) {
+        failure = trap_detect(sobj) != 0;
+    } else {
+        failure = gold_detect(sobj, FALSE) != 0;
+        if(sobj->blessed && (object_detect(sobj, GEM_CLASS) == 0))
+            failure = FALSE;
+    }
+    if (failure)
         *sobjp = 0; /* failure: strange_feeling() -> useup() */
 }
 

commit 0405f33957adc1705151c2146e4866790ac136a8

Fix: blessed gold detection could use up the scroll twice
This is an oversight introduced in commit 9cd266ae, which extended the
blessed scroll of gold detection to also find gems after it finds gold.
The issue is that both gold_detect and object_detect, if passed an
object pointer, might unconditionally delete the object (another
instance of unintuitive behavior where the deletion is hidden way down
in strange_feeling). So calling them twice on the same object pointer
is incorrect, but also avoiding gem detection if gold detection fails to
find anything is incorrect.

I resolved this by calling object_detect with a null pointer since it
didn't do anything special anyway when passed a blessed scroll of gold
detection. Also added another strange_feeling call but also with a null
pointer so it will simply deliver its message and not delete anything.

For posterity's sake this is the first crash detected in Junethack 2025.

src/read.c
@@ -2159,15 +2159,22 @@ seffect_gold_detection(struct obj **sobjp)
 {
     struct obj *sobj = *sobjp;
     boolean scursed = sobj->cursed;
+    boolean sblessed = sobj->blessed;
     boolean confused = (Confusion != 0);
     boolean failure;
 
     if (confused || scursed) {
         failure = trap_detect(sobj) != 0;
     } else {
+        /* the object_detect() for gems passes a null because
+         * 1) *sobjp might be null from gold_detect -> strange_feeling -> useup
+         * 2) the only logic in object_detect that uses the object is for cursed
+         *    objects or blessed potion/spell of object detection, none of which
+         *    apply here
+         */
         failure = gold_detect(sobj, FALSE) != 0;
-        if(sobj->blessed && (object_detect(sobj, GEM_CLASS) == 0))
-            failure = FALSE;
+        if (sblessed && (object_detect((struct obj *) 0, GEM_CLASS) != 0))
+            strange_feeling((struct obj *) 0, "You feel a lack of gems.");
     }
     if (failure)
         *sobjp = 0; /* failure: strange_feeling() -> useup() */